84207d69966c4074383ff8f80c35485ca859f9b1
Ansh Sharma <49720994+AnshSharmaa@users.noreply.github.com>
2021-04-11 15:01:37 +0530

    final fix, added comments

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+// this function takes the inputs and validates the date difference if all is right then it calls the api call function
 function check() {
     var name = document.getElementById("country").value;
     var sdate = document.getElementById("startdate").value;
@@ -10,27 +12,30 @@ function check() {
     }
 }
 
+// this function calls the api with the input data and if the data is found it calls the separate function
 function apicall(name, sdate, edate) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var res = this.responseText;
-            display(res);
+            separate(res);
         }
     };
     xhttp.open("GET", `https://api.covid19api.com/country/${name}?from=${sdate}T00:00:00Z&to=${edate}T00:00:00Zeg:-`, true);
     xhttp.send();
 }
 
-function display(res) {
+// this function breaks each result and sends it to the build function
+function separate(res) {
     //console.log(JSON.parse(res));
     JSON.parse(res).forEach(e => {
         build(e);
     });
 }
 
+// this function takes each result, extracts the relevant data and sends it to the html
 function build(e) {
-    console.log(e);
+    //console.log(e);
     //div recieves all the results
     var main = document.getElementById("result");
     //ndiv houses result of a day
